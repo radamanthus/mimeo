fb76eeaf660f1fc45dc9ad761692280a490f0f23
Radamanthus Batnag <radamanthus@gmail.com>
2012-09-05 13:56:32 +0800

    Add after_destroy callback

--- lib/mimeo.rb
+++ lib/mimeo.rb
@@ -6,24 +6,33 @@ module Mimeo
       cattr_accessor :ohm_model_class, :ohm_model_index
 
       self.ohm_model_class = model
-      self.ohm_model_index = options[:index]
 
       self.set_callback :save, :after do
         self.save_to_redis
+      end
+
+      self.set_callback :destroy, :after do
+        self.remove_from_redis
       end
     end
   end
 
   module InstanceMethods
+    def remove_from_redis
+      redis_record = get_redis_record
+      redis_record.delete
+      return true
+    end
+
     def save_to_redis
-      redis_record = get_redis_record(ohm_model_class, ohm_model_index)
+      redis_record = get_redis_record
       populate redis_record
       redis_record.save
       return true
     end
 
-    def get_redis_record(model, index)
-      model.find(rails_id: self.id).first || model.new
+    def get_redis_record
+      ohm_model_class.find(rails_id: self.id).first || ohm_model_class.new
     end
 
     def populate(record)
